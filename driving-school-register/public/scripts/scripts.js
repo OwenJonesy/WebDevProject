b3be709919449b7419a2116ffed917400728c481
OwenJonesy <C21377551@mytudublin.ie>
2023-11-18 21:15:46 +0000

    Login/Register

--- driving-school-register/public/scripts/scripts.js
+++ driving-school-register/public/scripts/scripts.js
@@ -1,5 +1,3 @@
-// scripts.js
-
 document.getElementById('registrationForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
@@ -27,3 +25,62 @@ document.getElementById('registrationForm').addEventListener('submit', async fun
         console.error('Error during registration:', error.message);
     }
 });
+
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
+    });
+
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formDataObject),
+        });
+
+        if (response.ok) {
+            console.log('Login successful');
+            showSuccessMessage(); // Display success message
+        } else {
+            console.error('Login failed:', response.statusText);
+            alert('Login failed. Please check your email and password.');
+        }
+    } catch (error) {
+        console.error('Error during login:', error.message);
+        alert('Error during login. Please try again later.');
+    }
+});
+
+// Toggle between registration and login forms
+function showRegistrationForm() {
+    document.getElementById('pageTitle').innerText = 'Driving School Registration';
+    document.getElementById('registrationForm').style.display = 'block';
+    document.getElementById('loginForm').style.display = 'none';
+    hideSuccessMessage(); // Hide success message when switching forms
+}
+
+function showLoginForm() {
+    document.getElementById('pageTitle').innerText = 'Driving School Login';
+    document.getElementById('registrationForm').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'block';
+    hideSuccessMessage(); // Hide success message when switching forms
+}
+
+// Display success message
+function showSuccessMessage() {
+    document.getElementById('successMessage').style.display = 'block';
+}
+
+// Hide success message
+function hideSuccessMessage() {
+    document.getElementById('successMessage').style.display = 'none';
+}
+
+// Initially show the registration form
+showRegistrationForm();
--- driving-school-register/server.js
+++ driving-school-register/server.js
@@ -1,5 +1,3 @@
-// server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -14,7 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.post('/register', async (req, res) => {
     try {
         if (!req.body || Object.keys(req.body).length === 0) {
-            // Check if the request body is empty
             return res.status(400).json({ error: 'Invalid request. Request body is empty.' });
         }
 
@@ -37,19 +34,46 @@ app.post('/register', async (req, res) => {
         const result = await studentsCollection.insertOne(req.body);
 
         if (result.insertedCount === 1) {
-            // Registration successful
             return res.status(201).json({ message: 'Registration successful', data: result.ops[0] });
         } else {
-            // No documents were inserted
             return res.status(500).json({ error: 'Internal Server Error' });
         }
     } catch (error) {
-        // Handle unexpected errors
+        console.error('Error during registration:', error);
         return res.status(500).json({ error: 'Internal Server Error' });
     } finally {
         closeDatabaseConnection();
     }
 });
+
+app.post('/login', async (req, res) => {
+    try {
+        const { loginEmail, loginPassword } = req.body;
+
+        if (!loginEmail || !loginPassword) {
+            return res.status(400).json({ error: 'Email and password are required for login.' });
+        }
+
+        const db = await connectToDatabase();
+        const studentsCollection = db.collection('students');
+
+        // Check if the user with the given email and password exists
+        const user = await studentsCollection.findOne({ email: loginEmail, password: loginPassword });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password.' });
+        }
+
+        // Login successful
+        return res.status(200).json({ message: 'Login successful', data: user });
+    } catch (error) {
+        console.error('Error during login:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        closeDatabaseConnection();
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
