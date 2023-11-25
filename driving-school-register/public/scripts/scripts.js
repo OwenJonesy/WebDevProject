c442372f04284f8d4009cfae737746abf5406967
OwenJonesy <C21377551@mytudublin.ie>
2023-11-25 21:24:00 +0000

    Database for Account done

--- driving-school-register/public/scripts/scripts.js
+++ driving-school-register/public/scripts/scripts.js
@@ -1,91 +1,25 @@
-// Registration form event listener
-document.getElementById('registrationForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    const formDataObject = {};
-    formData.forEach((value, key) => {
-        formDataObject[key] = value;
-    });
-
-    try {
-        const response = await fetch('/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formDataObject),
-        });
-
-        if (response.ok) {
-            const responseData = await response.json();
-
-            // Registration successful
-            console.log('Registration successful:', responseData.data);
-            showSuccessMessage('Registration successful');
-        } else {
-            const errorData = await response.json();
-
-            // Registration failed, show an error message
-            console.error('Registration failed:', errorData.error);
-            showErrorMessage(errorData.error);
-        }
-    } catch (error) {
-        console.error('Error during registration:', error.message);
-        showErrorMessage('Error during registration. Please try again later.');
-    }
-});
-
-// Login form event listener
-document.getElementById('loginForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    const formDataObject = {};
-    formData.forEach((value, key) => {
-        formDataObject[key] = value;
-    });
-
-    try {
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formDataObject),
-        });
-
-        if (response.ok) {
-            const responseData = await response.json();
-
-            // Login successful
-            console.log('Login successful:', responseData.data);
-            showSuccessMessage('Login successful');
-        } else {
-            const errorData = await response.json();
-
-            // Login failed, show an error message
-            console.error('Login failed:', errorData.error);
-            alert('Login failed. Please check your email and password.');
-        }
-    } catch (error) {
-        console.error('Error during login:', error.message);
-        alert('Error during login. Please try again later.');
-    }
-});
-
-// Event listener for the "Change Password" button
 document.getElementById('changePasswordButton').addEventListener('click', function () {
-    hideChangePasswordButton(); // Hide the "Change Password" button when showing the password update form
+    hideChangePasswordButton();
     document.getElementById('registrationForm').style.display = 'none';
     document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('deleteAccountForm').style.display = 'none';
     document.getElementById('updatePasswordForm').style.display = 'grid';
     document.getElementById('pageTitle').innerText = 'Change Password';
     document.getElementById('switchToLoginFormButton').style.display = 'inline-block';
     document.getElementById('switchToRegisterFormButton').style.display = 'inline-block';
 });
 
-// Add an event listener for the password update form
+document.getElementById('deleteAccountButton').addEventListener('click', function () {
+    hideDeleteAccountForm();
+    document.getElementById('registrationForm').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('updatePasswordForm').style.display = 'none';
+    document.getElementById('deleteAccountForm').style.display = 'grid';
+    document.getElementById('pageTitle').innerText = 'Delete Account';
+    document.getElementById('switchToLoginFormButton').style.display = 'inline-block';
+    document.getElementById('switchToRegisterFormButton').style.display = 'inline-block';
+});
+
 document.getElementById('updatePasswordForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
@@ -109,12 +43,10 @@ document.getElementById('updatePasswordForm').addEventListener('submit', async f
         });
 
         if (response.ok) {
-            // Password update successful
             const responseData = await response.json();
             console.log('Password update successful:', responseData.message);
             showSuccessMessage('Password update successful');
         } else {
-            // Password update failed, show an error message
             const errorData = await response.json();
             console.error('Password update failed:', errorData.error);
             showErrorMessage(errorData.error);
@@ -125,7 +57,42 @@ document.getElementById('updatePasswordForm').addEventListener('submit', async f
     }
 });
 
-// Function to hide the "Change Password" button
+document.getElementById('deleteAccountForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
+    });
+
+    try {
+        const response = await fetch('/delete-account', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: formDataObject.loginEmailForDelete,
+                password: formDataObject.loginPasswordForDelete,
+            }),
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log('Account deletion successful:', responseData.message);
+            showSuccessMessage('Account deletion successful');
+        } else {
+            const errorData = await response.json();
+            console.error('Account deletion failed:', errorData.error);
+            showErrorMessage(errorData.error);
+        }
+    } catch (error) {
+        console.error('Error during account deletion:', error.message);
+        showErrorMessage('Error during account deletion. Please try again later.');
+    }
+});
+
 function hideChangePasswordButton() {
     const changePasswordButton = document.getElementById('changePasswordButton');
     if (changePasswordButton) {
@@ -133,72 +100,53 @@ function hideChangePasswordButton() {
     }
 }
 
-// Function to show the "Change Password" button
-function showChangePasswordButton() {
-    const changePasswordButton = document.getElementById('changePasswordButton');
-    if (changePasswordButton) {
-        changePasswordButton.style.display = 'block';
+function hideDeleteAccountForm() {
+    const deleteAccountButton = document.getElementById('deleteAccountButton');
+    if (deleteAccountButton) {
+        deleteAccountButton.style.display = 'none';
     }
 }
 
-// Function to show success message
 function showSuccessMessage(message) {
-    hideErrorMessage(); // Hide any existing error message
+    hideErrorMessage();
     const successMessage = document.getElementById('successMessage');
     successMessage.innerText = message;
     successMessage.style.display = 'block';
 }
 
-// Function to hide success message
 function hideSuccessMessage() {
     document.getElementById('successMessage').style.display = 'none';
 }
 
-// Function to show error message
 function showErrorMessage(message) {
-    hideSuccessMessage(); // Hide any existing success message
+    hideSuccessMessage();
     const errorMessage = document.getElementById('errorMessage');
     errorMessage.innerText = message;
     errorMessage.style.display = 'block';
 }
 
-// Function to hide error message
 function hideErrorMessage() {
     document.getElementById('errorMessage').style.display = 'none';
 }
 
-// Function to switch to the registration form
 function showRegistrationForm() {
     document.getElementById('registrationForm').style.display = 'grid';
     document.getElementById('loginForm').style.display = 'none';
     document.getElementById('updatePasswordForm').style.display = 'none';
+    document.getElementById('deleteAccountForm').style.display = 'none';
     document.getElementById('pageTitle').innerText = 'Driving School Registration';
     document.getElementById('switchToLoginFormButton').style.display = 'inline-block';
     document.getElementById('switchToRegisterFormButton').style.display = 'none';
-    showChangePasswordButton(); // Show the "Change Password" button when switching to registration form
 }
 
-// Function to switch to the login form
 function showLoginForm() {
     document.getElementById('registrationForm').style.display = 'none';
     document.getElementById('loginForm').style.display = 'grid';
     document.getElementById('updatePasswordForm').style.display = 'none';
+    document.getElementById('deleteAccountForm').style.display = 'none';
     document.getElementById('pageTitle').innerText = 'Driving School Login';
     document.getElementById('switchToLoginFormButton').style.display = 'none';
     document.getElementById('switchToRegisterFormButton').style.display = 'inline-block';
-    showChangePasswordButton(); // Show the "Change Password" button when switching to login form
 }
 
-// Function to show the password update form
-function showPasswordUpdateForm() {
-    hideChangePasswordButton(); // Hide the "Change Password" button when showing the password update form
-    document.getElementById('registrationForm').style.display = 'none';
-    document.getElementById('loginForm').style.display = 'none';
-    document.getElementById('updatePasswordForm').style.display = 'grid';
-    document.getElementById('pageTitle').innerText = 'Change Password';
-    document.getElementById('switchToLoginFormButton').style.display = 'inline-block';
-    document.getElementById('switchToRegisterFormButton').style.display = 'inline-block';
-}
-
-// Initially show the registration form
 showRegistrationForm();
--- driving-school-register/server.js
+++ driving-school-register/server.js
@@ -1,5 +1,3 @@
-//server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -33,7 +31,6 @@ function closeDatabaseConnection() {
     console.log('Closed MongoDB connection');
 }
 
-// Middleware to connect to the database before handling requests
 app.use(async (req, res, next) => {
     try {
         database = await connectToDatabase();
@@ -43,105 +40,6 @@ app.use(async (req, res, next) => {
     }
 });
 
-// Routes for the different HTML pages
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'homepage.html'));
-});
-
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/bookings', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'bookings.html'));
-});
-
-app.get('/MyAccount', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'MyAccount.html'));
-});
-
-app.get('/Map', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Map.html'));
-});
-
-
-
-
-// Registration and login routes go here...
-
-
-
-
-app.post('/register', async (req, res) => {
-    try {
-        if (!req.body || Object.keys(req.body).length === 0) {
-            // Check if the request body is empty
-            return res.status(400).json({ error: 'Invalid request. Request body is empty.' });
-        }
-
-        const { email } = req.body;
-
-        if (!email) {
-            return res.status(400).json({ error: 'Email is required for registration.' });
-        }
-
-        const db = await connectToDatabase();
-        const studentsCollection = db.collection('students');
-
-        // Check if the email already exists
-        const existingUser = await studentsCollection.findOne({ email });
-
-        if (existingUser) {
-            // Email already exists, send an error response
-            return res.status(409).json({ error: 'Email already exists. Please use a different email.' });
-        }
-
-        const result = await studentsCollection.insertOne(req.body);
-
-        if (result.insertedCount === 1) {
-            // Registration successful
-            return res.status(201).json({ message: 'Registration successful', data: result.ops[0] });
-        } else {
-            // No documents were inserted
-           // return res.status(500).json({ error: 'Internal Server Error' });
-        }
-    } catch (error) {
-        // Handle unexpected errors
-        //return res.status(500).json({ error: 'Internal Server Error' });
-    } finally {
-        closeDatabaseConnection();
-    }
-});
-
-app.post('/login', async (req, res) => {
-    try {
-        const { loginEmail, loginPassword } = req.body;
-
-        if (!loginEmail || !loginPassword) {
-            return res.status(400).json({ error: 'Email and password are required for login.' });
-        }
-
-        const db = await connectToDatabase();
-        const studentsCollection = db.collection('students');
-
-        // Check if the user with the given email and password exists
-        const user = await studentsCollection.findOne({ email: loginEmail, password: loginPassword });
-
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid email or password.' });
-        }
-
-        // Login successful
-        return res.status(200).json({ message: 'Login successful', data: user });
-    } catch (error) {
-        console.error('Error during login:', error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-    } finally {
-        closeDatabaseConnection();
-    }
-});
-
-// Add a new route for password update
 app.post('/update-password', async (req, res) => {
     try {
         const { email, oldPassword, newPassword } = req.body;
@@ -153,25 +51,53 @@ app.post('/update-password', async (req, res) => {
         const db = await connectToDatabase();
         const studentsCollection = db.collection('students');
 
-        // Check if the user with the given email and old password exists
         const user = await studentsCollection.findOne({ email, password: oldPassword });
 
         if (!user) {
-            return res.status(401).json({ error: 'Invalid email or old password.' });
+            return res.status(401).json({ error: 'Invalid email or old password for password update.' });
         }
 
-        // Update the user's password
         const updateResult = await studentsCollection.updateOne({ email }, { $set: { password: newPassword } });
 
         if (updateResult.modifiedCount === 1) {
-            // Password update successful
             return res.status(200).json({ message: 'Password update successful' });
         } else {
-            // No documents were modified
             return res.status(500).json({ error: 'Internal Server Error' });
         }
     } catch (error) {
         console.error('Error during password update:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        closeDatabaseConnection();
+    }
+});
+
+app.post('/delete-account', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Email and password are required for account deletion.' });
+        }
+
+        const db = await connectToDatabase();
+        const studentsCollection = db.collection('students');
+
+        const user = await studentsCollection.findOne({ email, password });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password for account deletion.' });
+        }
+
+        const deletionResult = await studentsCollection.deleteOne({ email, password });
+
+        if (deletionResult.deletedCount === 1) {
+            return res.status(200).json({ message: 'Account deletion successful' });
+        } else {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } catch (error) {
+        console.error('Error during account deletion:', error);
         return res.status(500).json({ error: 'Internal Server Error' });
     } finally {
         closeDatabaseConnection();
